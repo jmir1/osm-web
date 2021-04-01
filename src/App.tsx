3de92b84969e7dc9648ef88e307a9ca6c4c33b35
Luke Van Epen <vanweapon@gmail.com>
2021-04-01 13:28:14 +1100

    centerd the div

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import * as React from "react";
-import { ChakraProvider, Box, VStack, Grid, theme } from "@chakra-ui/react";
+import { ChakraProvider, Box, VStack, Grid, theme, Heading, Center } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import OSMRankings from "./components/OSMRankings";
 
 export const App = () => (
 	<ChakraProvider theme={theme}>
-		<Box textAlign="center" fontSize="xl">
-			<Grid minH="100vh" p={3}>
-				<ColorModeSwitcher justifySelf="flex-end" />
-				<VStack spacing={8}>
-					<OSMRankings />
-				</VStack>
-			</Grid>
-		</Box>
+		<Center>
+			<Box textAlign="center" fontSize="xl">
+				<Grid minH="100vh" p={3}>
+					<ColorModeSwitcher justifySelf="flex-end" />
+					<VStack spacing={8}>
+						<Heading>Rankings</Heading>
+						<OSMRankings />
+					</VStack>
+				</Grid>
+			</Box>
+		</Center>
 	</ChakraProvider>
 );
